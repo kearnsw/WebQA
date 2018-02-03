21c3cdfa42c2365c9f6353bf24b3169a3a0a0597
Zeyu Liu <zeyuliu2@uw.edu>
2018-02-02 23:28:31 -0800

    model

--- models/run.py
+++ models/run.py
@@ -12,8 +12,15 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(os.path.join(__file__, os.par
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
                     level=LEVEL)
 
-from allennlp.commands import main  # pylint: disable=wrong-import-position
+from memory_profiler import profile
+import cProfile
+
+@profile
+def profWrap():
+    cProfile.run('from allennlp.commands.train import train_model_from_file')
+    cProfile.run('train_model_from_file("./model/baseline.json", "./result")')
+
 
 
 if __name__ == "__main__":
-    main(prog="python -m allennlp.run")
+    profWrap()
